a0b23c6f90c12217055f79b784fc0ad6d179381d
jtrampe1 <johntrampe07@yahoo.com>
2018-01-24 09:23:01 -0700

    string manipulations

--- Week03/string-manipulations/js/main.js
+++ Week03/string-manipulations/js/main.js
@@ -1,6 +1,33 @@
 var urlTemplate = 'http://placehold.it/:size/:color/:background/';
 var urlQueryStringTemplate = "?text="
 
+var texInput = document.querySelector('input[name=placeholderText]');
+
+
+function onplaceholderTextChanged(evt) {
+    // The .target of the evt is the object that 'experienced' the event
+    // The .value is an attribute of the <input> element
+    console.log(evt.target.value); 
+
+    var imgElement = document.querySelector('img');
+    var src = imgElement.src;
+    var indexOfEqualSign = src.indexOf('=');
+    var newSrc = src.substring(0, indexOfEqualSign);
+
+    console.log(newSrc);
+    newSrc = newSrc + '=' + evt.target.value;
+    imgElement.src = newSrc;
+
+}
+
+
+// Wire up the event handler
+texInput.addEventListener('change', onplaceholderTextChanged);
+
+
+
+
+
 // mini-lesson on string manipulation:
 
 //  split string into an array
